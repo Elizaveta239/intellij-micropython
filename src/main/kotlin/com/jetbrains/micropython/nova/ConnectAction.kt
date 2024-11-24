42ba73617af83c48f2076166a397c4e0f165b382
Lukáš Kremla <155779787+lukaskremla@users.noreply.github.com>
2024-11-24 09:07:57 +0100

    Fixed a cannot x can not typo

--- src/main/kotlin/com/jetbrains/micropython/nova/ConnectAction.kt
+++ src/main/kotlin/com/jetbrains/micropython/nova/ConnectAction.kt
@@ -63,7 +63,7 @@ suspend fun doConnect(fileSystemWidget: FileSystemWidget) {
             val result = Messages.showIdeaMessageDialog(
                 fileSystemWidget.project,
                 msg,
-                "Can Not Connect",
+                "Cannot Connect",
                 arrayOf("OK", "Settings..."),
                 1,
                 AllIcons.General.ErrorDialog,
